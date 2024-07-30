8badd6f63c5b389900dff6cebc42b9461c4e576e
Raul Baena <raul.baena@bcncgroup.com>
2024-07-30 20:37:37 +0200

    ww

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,37 @@
-terraform  {
-    required_providers {
-        proxmox = {
-            source = "Telmate/proxmox"
-            version = ">=2.9.0"
-        }
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = ">=2.9.0"
     }
+  }
 }
 
 provider "proxmox" {
-    pm_api_url = vm.pm_api_url
-    pm_user = var.pm_user
-    pm_password = var.pm_password
-    pm_tls_insecure = true
+  pm_api_url      = var.pm_api_url
+  pm_user         = var.pm_user
+  pm_password     = var.pm_password
+  pm_tls_insecure = true
 }
 
-variable "pm_api_url" {}
-variable "pm_user" {}
-variable "pm_password" {}
-
-resource  "proxmox_vm_qemu" "k8s_node" {
-    count = 2
-    name = "k8s-node-${count.index + 1}"
-    target_node = "pve"
-    clone = "ubuntu_template"
-    cores = 1
-    memory = 2048
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-    }
-
-    disk {
-        size = "15G"
-        type = "scsi"
-        storage = "local-lvm"
-    }
-    sshkeys = file("~/.ssh/id_rsa.pub")
-    os_type = "cloud-init"
-    ciuser = "ubuntu"
-    cipassword = "password"
+resource "proxmox_vm_qemu" "k8s_node" {
+  count = 2
+  name  = "k8s-node-${count.index + 1}"
+  target_node = "pve"
+  clone = "ubuntu-template"
+  cores = 1
+  memory = 2048
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  disk {
+    size    = "15G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+  sshkeys = file("~/.ssh/id_rsa.pub")
+  os_type = "cloud-init"
+  ciuser = "ubuntu"
+  cipassword = "password"
 }
-
